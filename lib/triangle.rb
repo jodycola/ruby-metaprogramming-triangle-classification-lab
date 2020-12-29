a01d2fd8dd3c37e559cc8b49029074e4007942e0
Kody Samaroo <github email address>
2020-12-28 19:14:58 -0500

    commit tree 44d6c2f3eebb0fe5b7a901297820b49f3f2b1e66

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,54 @@
 class Triangle
   # write code here
+  attr_accessor = :a, :b, :c
+
+  @@um = []
+
+  def initalize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @sum = [a,b,c]
+  end
+
+  def kind
+    if(negative? == true || valid? == false)
+      raise TriangleError
+    elsif(equilateral?)
+      :equilateral
+    elsif(scalene?)
+      :scalene
+    elsif(isosceles?)
+      :isosceles
+    end
+  end
+
+  def negative?
+    @sum.each do |length|
+      if(length <= 0)
+        return true
+      end
+    end
+  end
+
+  def valid?
+    (@a + @b > @c) && (@b + @c > @a) && (@a + @c > @b)
+  end
+
+  def equilateral?
+    (@a == @b) && (@a == @c)
+  end
+
+   def scalene?
+    (@a != @b) && (@a!= @c) && (@b != @c)
+  end
+
+   def isosceles?
+    (@b == @c) || (@a == @b) || (@a = @c)
+  end
+
+  class TriangleError < StandardError
+    puts "This is not a triangle."
+  end
+  
 end
